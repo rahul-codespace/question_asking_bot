bedfd9a0b8f69c9b822be98c99ede7d38014aedc
RAHUL-COD3 <rahul@promactinfo.com>
2024-07-05 14:33:19 +0530

    Update host url

--- src/main.py
+++ src/main.py
@@ -22,4 +22,4 @@ async def start_conversation(user_name: str, inputDto: ConversationInputDto):
     return ConversationOutputDto(message=result, correct_responses=saturn_bot.correct_responses)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
